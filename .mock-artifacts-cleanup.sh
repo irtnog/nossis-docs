b4dcc59eea3eee1de058e06691b94e86f5aec17c
Matthew X. Economou <xenophon@irtnog.org>
2024-12-29 14:22:07 -0500

    style: mock up missing OpenTofu deployment artifacts when linting

---
+++ .mock-artifacts-cleanup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# nossis-docs, serverless hosting for static, private web sites that
+# works like GitHub Pages
+#
+# Copyright (C) 2024  Matthew X. Economou
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+
+# Remove mock deployment artifacts (if any); cf.
+# .pre-commit-config.yaml.
+
+if [ -f lambda-functions.zip -a ! -s lambda-functions.zip ]; then
+    rm lambda-functions.zip
+fi
